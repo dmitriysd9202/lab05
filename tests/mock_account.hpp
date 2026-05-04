c56d83f82b7a10385f20cba30ca7365ff7feab60
dmitriysd9202 <dmitriysd9202@gmail.com>
2026-05-04 21:24:01 +0000

    Fix mock syntax for gtest 1.8.1

--- tests/mock_account.hpp
+++ tests/mock_account.hpp
@@ -6,9 +6,9 @@ class MockAccount : public Account {
 public:
     MockAccount(int id, const std::string& owner, double balance)
         : Account(id, owner, balance) {}
-    MOCK_METHOD(void, deposit, (double amount), (override));
-    MOCK_METHOD(void, withdraw, (double amount), (override));
-    MOCK_METHOD(double, getBalance, (), (const, override));
-    MOCK_METHOD(int, getId, (), (const, override));
-    MOCK_METHOD(std::string, getOwner, (), (const, override));
+    MOCK_METHOD1(deposit, void(double));
+    MOCK_METHOD1(withdraw, void(double));
+    MOCK_CONST_METHOD0(getBalance, double());
+    MOCK_CONST_METHOD0(getId, int());
+    MOCK_CONST_METHOD0(getOwner, std::string());
 };
